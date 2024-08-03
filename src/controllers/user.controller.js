e858a5d1f1a44e6f6d6092359ad5c702390ab128
ITZBaburam <139737597+Baburam99@users.noreply.github.com>
2024-08-03 08:01:11 +0545

    added subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -166,7 +166,7 @@ const logoutUser = asynchandler(async (req, res) => {
 const refreshAccessToken = asynchandler(async (req, res) => {
   const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "unauthorizes request");
   }
 
